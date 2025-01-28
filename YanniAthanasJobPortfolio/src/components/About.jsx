2aecd2660962165b8881b5aadaacf064ca4b5665
Yanni Athanasoulias <yanni.athanasoulias@dawsoncollege.qc.ca>
2025-01-27 23:35:34 -0500

    Change file path to image

--- YanniAthanasJobPortfolio/src/components/About.jsx
+++ YanniAthanasJobPortfolio/src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
       <h1> About me </h1>
         <section id="about">
           <p id="About-Professional"> I am a student currently studying in computer science at Dawson, doing my best to improve my skills and develop great work habits </p>
-          <img src="../../public/PortfolioImg.jpg" id="profileImg" />
+          <img src="/PortfolioImg.jpg" id="profileImg" />
         </section>
     </section>
     </>
